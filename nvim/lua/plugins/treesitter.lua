5d62dabd41353b1edc2fbc2375d4f7f9587e0b53
zachjud <zjudson@protonmail.com>
2025-01-22 13:49:05 -0500

    Changed Treesitter foldlevelstart to 2

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -19,4 +19,4 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldtext = ""
 vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 1
+vim.opt.foldlevelstart = 2
