6df1be6d2c9fb12b7a2208aa4eb463c41143a67c
zachjud <zjudson@protonmail.com>
2025-02-12 13:12:03 -0500

    Fixed comment formatting in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,19 +4,19 @@ require('plugins')
 -- Remaps --
 require('remaps')
 
--- Filetype Detection --
+-- Special Filetype Detection
 require('ftdetect')
 
--- Colorscheme --
+-- Colorscheme
 vim.cmd.colorscheme('tokyonight-night')
 vim.g.terminal_color_0 = vim.g.terminal_color_8 -- Fix too dark black
 
 -- Options --
 
--- Clipboard --
+-- Clipboard
 vim.opt.clipboard = "unnamedplus"
 
--- View --
+-- View
 vim.opt.relativenumber = true
 vim.opt.number = true
 vim.opt.cursorline = true
@@ -24,20 +24,21 @@ vim.opt.cursorlineopt = 'both'
 vim.opt.signcolumn = 'yes'
 vim.opt.conceallevel = 2
 
--- Indentation --
+-- Indentation
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 
--- Undo --
+-- Undo
 vim.opt.undodir = os.getenv("HOME") .. '/.nvim/undodir'
 vim.opt.undofile = true
 
--- Search --
+-- Search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.mouse = 'a'
 
-vim.opt.fillchars = { eob = ' ' } -- Remove EOF characters
+-- Remove EOF characters
+vim.opt.fillchars = { eob = ' ' }
