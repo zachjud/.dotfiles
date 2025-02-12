169ef19bacf4ea86bab376bdeaa4ead5e8552916
zachjud <zjudson@protonmail.com>
2025-02-12 13:14:15 -0500

    Added undohistory plugin to telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,7 +6,8 @@ require('telescope').setup {
       override_file_sorter = true,     -- override the file sorter
       case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
                                        -- the default case_mode is "smart_case"
-    }
+    },
+    undo = { }
   },
   pickers = {
     live_grep = {
